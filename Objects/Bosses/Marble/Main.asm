7acbca649e3dba93612d1ad50e1ada63a9466cc6
Ezra <fuzzy-bit@users.noreply.github.com>
2023-03-01 23:18:35 -0500

    MZ boss progress

--- Objects/Bosses/Marble/Main.asm
+++ Objects/Bosses/Marble/Main.asm
@@ -4,47 +4,53 @@
 BossMarble:
 		moveq	#0, d0
 		move.b	obRoutine(a0), d0
-		move.w	Obj73_Index(pc, d0.w), d1
-		jmp	Obj73_Index(pc, d1.w)
+		move.w	@Index(pc, d0.w), d1
+		jmp	@Index(pc, d1.w)
 ; ===========================================================================
-Obj73_Index:	dc.w Obj73_Main-Obj73_Index
-		dc.w Obj73_ShipMain-Obj73_Index
-		dc.w Obj73_FaceMain-Obj73_Index
-		dc.w Obj73_FlameMain-Obj73_Index
-		dc.w Obj73_TubeMain-Obj73_Index
+Obj73_TargetX:			equ $30
+Obj73_TargetY:			equ $38
 
-Obj73_ObjData:	
+Obj73_FaceStatus:		equ $34
+Obj73_FlashTimer:		equ $3E
+
+@Index:	
+		dc.w @Main-@Index
+		dc.w @ShipMain-@Index
+		dc.w Obj73_FaceMain-@Index
+		dc.w Obj73_FlameMain-@Index
+		dc.w Obj73_TubeMain-@Index
+
+@ObjData:	
 		; routine number, animation, sprite priority
 		dc.b 2, 0, 4	; Ship
 		dc.b 4, 1, 4	; Face
 		dc.b 6, 7, 4	; Flame
 		dc.b 8, 0, 3	; Tube
 
-Obj73_FaceStatus:		equ $34
 ; ===========================================================================
 
-Obj73_Main:	; Routine 0
-		move.w	obX(a0), $30(a0)
-		move.w	obY(a0), $38(a0)
+@Main:	; Routine 0
+		move.w	obX(a0), Obj73_TargetX(a0)
+		move.w	obY(a0), Obj73_TargetY(a0)
 		move.b	#$F, obColType(a0)
 		move.b	#8, obColProp(a0) ; set number of hits to 8
 
-		lea	Obj73_ObjData(pc), a2
+		lea	@ObjData(pc), a2 ; setup sprites
 		movea.l	a0, a1
 		moveq	#3, d1
 		
-		bra.s	Obj73_LoadBoss
+		bra.s	@LoadBoss
 ; ===========================================================================
 
-Obj73_Loop:
+@Loop:
 		jsr	(FindNextFreeObj).l
-		bne.s	Obj73_ShipMain
+		bne.s	@ShipMain
 
 		move.b	#id_BossMarble, 0(a1)
 		move.w	obX(a0), obX(a1)
 		move.w	obY(a0), obY(a1)
 
-Obj73_LoadBoss:
+@LoadBoss:
 		bclr	#0, obStatus(a0)
 		clr.b	ob2ndRout(a1)
 		
@@ -59,15 +65,15 @@ Obj73_LoadBoss:
 
 		move.l	a0, Obj73_FaceStatus(a1)
 		
-		dbf	d1, Obj73_Loop	; repeat sequence 3 more times
+		dbf	d1, @Loop	; repeat sequence 3 more times
 
-Obj73_ShipMain:	; Routine 2
+@ShipMain:	; Routine 2
 		moveq	#0, d0
 
 		move.b	ob2ndRout(a0), d0
-		move.w	Obj73_ShipIndex(pc, d0.w), d1
+		move.w	@ShipIndex(pc, d0.w), d1
 
-		jsr	Obj73_ShipIndex(pc, d1.w)
+		jsr	@ShipIndex(pc, d1.w)
 		lea	(Ani_Eggman).l, a1
 
 		jsr	(AnimateSprite).l
@@ -79,268 +85,292 @@ Obj73_ShipMain:	; Routine 2
 		
 		jmp	(DisplaySprite).l
 ; ===========================================================================
-Obj73_ShipIndex:dc.w Obj73_MoveIn-Obj73_ShipIndex
-		dc.w loc_183AA-Obj73_ShipIndex
-		dc.w loc_184F6-Obj73_ShipIndex
-		dc.w loc_1852C-Obj73_ShipIndex
-		dc.w loc_18582-Obj73_ShipIndex
+@ShipIndex:
+		dc.w @MoveIn-@ShipIndex
+		dc.w @RunBoss-@ShipIndex
+		dc.w @OnDefeated-@ShipIndex
+		dc.w @Flee-@ShipIndex
+		dc.w @loc_18582-@ShipIndex
 ; ===========================================================================
 
-Obj73_MoveIn:
+@MoveIn:
 		move.b	$3F(a0), d0
 		addq.b	#2, $3F(a0)
 		jsr	(CalcSine).l
+
 		asr.w	#2, d0
 		move.w	d0, obVelY(a0)
 		move.w	#-$100, obVelX(a0)
 		bsr.w	BossMove
-		cmpi.w	#$1910, $30(a0)
-		bne.s	Obj73_IntroRandomFace
+
+		cmpi.w	#$1910, Obj73_TargetX(a0) ; are we done with the intro?
+		bne.s	@IntroRandomFace ; fuck you o'm straight
 
 		addq.b	#2, ob2ndRout(a0)
 		clr.b	obSubtype(a0)
 		clr.l	obVelX(a0)
 
-Obj73_IntroRandomFace:
+@IntroRandomFace:
 		jsr	(RandomNumber).l
 		move.b	d0, Obj73_FaceStatus(a0)
 
-loc_1833E:
-		move.w	$38(a0), obY(a0)
-		move.w	$30(a0), obX(a0)
-		cmpi.b	#4, ob2ndRout(a0)
-		bcc.s	locret_18390
-		tst.b	obStatus(a0)
-		bmi.s	loc_18392
-		tst.b	obColType(a0)
-		bne.s	locret_18390
-		tst.b	$3E(a0)
-		bne.s	loc_18374
-		move.b	#$28, $3E(a0)
+@CheckHit:
+		move.w	Obj73_TargetY(a0), obY(a0) ; apply our target position
+		move.w	Obj73_TargetX(a0), obX(a0)
+
+		cmpi.b	#4, ob2ndRout(a0) ; are we on the face routine?
+		bcc.s	@rts
+
+		tst.b	obStatus(a0) ; has boss been defeated?
+		bmi.s	@Defeat ; hell yea
+
+		tst.b	obColType(a0) ; check the collision type, no duplicate hits
+		bne.s	@rts
+
+		tst.b	Obj73_FlashTimer(a0) ; still flashing?
+		bne.s	@Flash ; eye candy!
+
+		move.b	#$28, Obj73_FlashTimer(a0) ; set flash timer
 		sfx	sfx_BossHit	; play boss damage sound
 
-loc_18374:
+@Flash:
 		lea	(v_pal_dry+$22).w, a1
 		moveq	#0, d0
+
 		tst.w	(a1)
-		bne.s	loc_18382
+		bne.s	@SetCollisionOnFlash
+
 		move.w	#cWhite, d0
 
-loc_18382:
+@SetCollisionOnFlash:
 		move.w	d0, (a1)
-		subq.b	#1, $3E(a0)
-		bne.s	locret_18390
+		subq.b	#1, Obj73_FlashTimer(a0)
+		bne.s	@rts ; gtfo if flash timer is over
+		
 		move.b	#$F, obColType(a0)
 
-locret_18390:
-		rts	
+@rts:
+		rts
 ; ===========================================================================
 
-loc_18392:
+@Defeat:
 		moveq	#100, d0
 		bsr.w	AddPoints
+		
 		move.b	#4, ob2ndRout(a0)
 		move.w	#$B4, $3C(a0)
 		clr.w	obVelX(a0)
+
 		rts	
 ; ===========================================================================
 
-loc_183AA:
+@RunBoss:
 		moveq	#0, d0
 		move.b	obSubtype(a0), d0
-		move.w	off_183C2(pc, d0.w), d0
-		jsr	off_183C2(pc, d0.w)
+		move.w	@ShipIndex2(pc, d0.w), d0
+
+		jsr		@ShipIndex2(pc, d0.w)
+		
 		andi.b	#6, obSubtype(a0)
-		bra.w	loc_1833E
+		bra.w	@CheckHit
 ; ===========================================================================
-off_183C2:	dc.w loc_183CA-off_183C2
-		dc.w Obj73_MakeLava2-off_183C2
-		dc.w loc_183CA-off_183C2
-		dc.w Obj73_MakeLava2-off_183C2
+@ShipIndex2:	
+		dc.w @ControlDirection-@ShipIndex2
+		dc.w @MakeBottomLava-@ShipIndex2
+		dc.w @ControlDirection-@ShipIndex2
+		dc.w @MakeBottomLava-@ShipIndex2
 ; ===========================================================================
 
-loc_183CA:
-		tst.w	obVelX(a0)
-		bne.s	loc_183FE
+@ControlDirection:
+		tst.w	obVelX(a0) ; is eggman not moving?
+		bne.s	@CheckFireball ; gang shit
+		
 		moveq	#$40, d0
-		cmpi.w	#$22C, $38(a0)
-		beq.s	loc_183E6
-		bcs.s	loc_183DE
+		cmpi.w	#$22C, Obj73_TargetY(a0) ; have we reached the other side?
+		beq.s	@SwapSides ; we're good to go
+		bcs.s	@ContinueMoving ; less than $22C, keep going
+
 		neg.w	d0
 
-loc_183DE:
+@ContinueMoving:
 		move.w	d0, obVelY(a0)
 		bra.w	BossMove
 ; ===========================================================================
 
-loc_183E6:
+@SwapSides:
 		move.w	#$200, obVelX(a0)
 		move.w	#$100, obVelY(a0)
-		btst	#0, obStatus(a0)
-		bne.s	loc_183FE
+		btst	#0, obStatus(a0) ; is bit 0 clear
+		bne.s	@CheckFireball ; if true, check if we should shoot fireball
+
 		neg.w	obVelX(a0)
 
-loc_183FE:
-		cmpi.b	#$18, $3E(a0)
-		bcc.s	Obj73_MakeLava
+@CheckFireball:
+		cmpi.b	#$18, Obj73_FlashTimer(a0) ; shoot fireball if flash timer >= #$18
+		bcc.s	@FireLava ; pew pew
+		
 		bsr.w	BossMove
 		subq.w	#4, obVelY(a0)
 
-Obj73_MakeLava:
+@FireLava:
 		subq.b	#1, Obj73_FaceStatus(a0)
-		bcc.s	loc_1845C
+		bcc.s	@IsShipRight
+
 		jsr	(FindFreeObj).l
-		bne.s	loc_1844A
+		bne.s	@RandomFace
 		move.b	#id_LavaBall, 0(a1) ; load lava ball object
 		move.w	#$2E8, obY(a1)	; set Y	position
+
 		jsr	(RandomNumber).l
 		andi.l	#$FFFF, d0
 		divu.w	#$50, d0
 		swap	d0
+
 		addi.w	#$1878, d0
 		move.w	d0, obX(a1)
 		lsr.b	#7, d1
 		move.w	#$FF, obSubtype(a1)
 
-loc_1844A:
+@RandomFace:
 		jsr	(RandomNumber).l
 		andi.b	#$1F, d0
 		addi.b	#$40, d0
 		move.b	d0, Obj73_FaceStatus(a0)
 
-loc_1845C:
+@IsShipRight:
 		btst	#0, obStatus(a0)
-		beq.s	loc_18474
-		cmpi.w	#$1910, $30(a0)
-		blt.s	locret_1849C
-		move.w	#$1910, $30(a0)
-		bra.s	loc_18482
+		beq.s	@IsShipLeft
+		cmpi.w	#$1910, Obj73_TargetX(a0)
+		blt.s	@FireLava_rts
+		move.w	#$1910, Obj73_TargetX(a0)
+		bra.s	@Dive
 ; ===========================================================================
 
-loc_18474:
-		cmpi.w	#$1830, $30(a0)
-		bgt.s	locret_1849C
-		move.w	#$1830, $30(a0)
+@IsShipLeft:
+		cmpi.w	#$1830, Obj73_TargetX(a0)
+		bgt.s	@FireLava_rts
+		move.w	#$1830, Obj73_TargetX(a0)
 
-loc_18482:
+@Dive:
 		clr.w	obVelX(a0)
 		move.w	#-$180, obVelY(a0)
-		cmpi.w	#$22C, $38(a0)
-		bcc.s	loc_18498
+		cmpi.w	#$22C, Obj73_TargetY(a0)
+		bcc.s	@StartFiring
 		neg.w	obVelY(a0)
 
-loc_18498:
-		addq.b	#2, obSubtype(a0)
+@StartFiring:
+		addq.b	#2, obSubtype(a0) ; make the fireball
 
-locret_1849C:
+@FireLava_rts:
 		rts	
 ; ===========================================================================
 
-Obj73_MakeLava2:
+@MakeBottomLava:
 		bsr.w	BossMove
-		move.w	$38(a0), d0
+		move.w	Obj73_TargetY(a0), d0
 		subi.w	#$22C, d0
-		bgt.s	locret_184F4
+		bgt.s	@MakeBottomLava_rts
 		move.w	#$22C, d0
 		tst.w	obVelY(a0)
-		beq.s	loc_184EA
+		beq.s	@CancelBottomLava
 		clr.w	obVelY(a0)
 		move.w	#$50, $3C(a0)
 		bchg	#0, obStatus(a0)
 		jsr	(FindFreeObj).l
-		bne.s	loc_184EA
-		move.w	$30(a0), obX(a1)
-		move.w	$38(a0), obY(a1)
+		bne.s	@CancelBottomLava
+		move.w	Obj73_TargetX(a0), obX(a1)
+		move.w	Obj73_TargetY(a0), obY(a1)
 		addi.w	#$18, obY(a1)
 		move.b	#id_BossFire, (a1)	; load lava ball object
 		move.b	#1, obSubtype(a1)
 
-loc_184EA:
+@CancelBottomLava:
 		subq.w	#1, $3C(a0)
-		bne.s	locret_184F4
+		bne.s	@MakeBottomLava_rts
 		addq.b	#2, obSubtype(a0)
 
-locret_184F4:
+@MakeBottomLava_rts:
 		rts	
 ; ===========================================================================
 
-loc_184F6:
+@OnDefeated:
 		subq.w	#1, $3C(a0)
-		bmi.s	loc_18500
+		bmi.s	@StartFleeing
 		bra.w	BossDefeated
 ; ===========================================================================
 
-loc_18500:
-		bset	#0, obStatus(a0)
+@StartFleeing:
+		bset	#0, obStatus(a0) ; flip eggman's sprite
 		bclr	#7, obStatus(a0)
+		
 		clr.w	obVelX(a0)
 		addq.b	#2, ob2ndRout(a0)
 		move.w	#-$26, $3C(a0)
 		tst.b	(v_bossstatus).w
-		bne.s	locret_1852A
+		bne.s	@StartFleeing_rts
 		move.b	#1, (v_bossstatus).w
 		clr.w	obVelY(a0)
 
-locret_1852A:
+@StartFleeing_rts:
 		rts	
 ; ===========================================================================
 
-loc_1852C:
+@Flee:
 		addq.w	#1, $3C(a0)
-		beq.s	loc_18544
-		bpl.s	loc_1854E
-		cmpi.w	#$270, $38(a0)
-		bcc.s	loc_18544
+		beq.s	@loc_18544
+		bpl.s	@loc_1854E
+		cmpi.w	#$270, Obj73_TargetY(a0)
+		bcc.s	@loc_18544
 		addi.w	#$18, obVelY(a0)
-		bra.s	loc_1857A
+		bra.s	@loc_1857A
 ; ===========================================================================
 
-loc_18544:
+@loc_18544:
 		clr.w	obVelY(a0)
 		clr.w	$3C(a0)
-		bra.s	loc_1857A
+		bra.s	@loc_1857A
 ; ===========================================================================
 
-loc_1854E:
+@loc_1854E:
 		cmpi.w	#$30, $3C(a0)
-		bcs.s	loc_18566
-		beq.s	loc_1856C
+		bcs.s	@loc_18566
+		beq.s	@loc_1856C
 		cmpi.w	#$38, $3C(a0)
-		bcs.s	loc_1857A
+		bcs.s	@loc_1857A
 		addq.b	#2, ob2ndRout(a0)
-		bra.s	loc_1857A
+		bra.s	@loc_1857A
 ; ===========================================================================
 
-loc_18566:
+@loc_18566:
 		subq.w	#8, obVelY(a0)
-		bra.s	loc_1857A
+		bra.s	@loc_1857A
 ; ===========================================================================
 
-loc_1856C:
+@loc_1856C:
 		clr.w	obVelY(a0)
 		music	mus_MZ		; play MZ music
 
-loc_1857A:
+@loc_1857A:
 		bsr.w	BossMove
-		bra.w	loc_1833E
+		bra.w	@CheckHit
 ; ===========================================================================
 
-loc_18582:
+@loc_18582:
 		move.w	#$500, obVelX(a0)
 		move.w	#-$40, obVelY(a0)
 		cmpi.w	#$1960, (v_limitright2).w
-		bcc.s	loc_1859C
+		bcc.s	@loc_1859C
 		addq.w	#2, (v_limitright2).w
-		bra.s	loc_185A2
+		bra.s	@loc_185A2
 ; ===========================================================================
 
-loc_1859C:
+@loc_1859C:
 		tst.b	obRender(a0)
 		bpl.s	Obj73_ShipDel
 
-loc_185A2:
+@loc_185A2:
 		bsr.w	BossMove
-		bra.w	loc_1833E
+		bra.w	@CheckHit
 ; ===========================================================================
 
 Obj73_ShipDel:
