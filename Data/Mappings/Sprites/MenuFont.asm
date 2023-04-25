5abcd5dee02319592c85d2c6ac58fdd6a6080468
Ezra <fuzzy-bit@users.noreply.github.com>
2023-04-24 20:31:00 -0400

    Re-added menu font

--- Data/Mappings/Sprites/MenuFont.asm
+++ Data/Mappings/Sprites/MenuFont.asm
@@ -1,5 +1,17 @@
 MenuFont:
-MenuFont_0: 	dc.w MenuFont_2-MenuFont
-MenuFont_2: 	dc.b $1
-	dc.b $FC, $0, $0, $1F, $FC
+MenuFont_0: 	dc.w MenuFont_A-MenuFont
+MenuFont_2: 	dc.w MenuFont_12-MenuFont
+MenuFont_4: 	dc.w MenuFont_1A-MenuFont
+MenuFont_6: 	dc.w MenuFont_22-MenuFont
+MenuFont_8: 	dc.w MenuFont_2A-MenuFont
+MenuFont_A: 	dc.b $0, $1
+	dc.b $FC, $0, $0, $11, $FF, $FC
+MenuFont_12: 	dc.b $0, $1
+	dc.b $FC, $0, $0, $12, $FF, $FC
+MenuFont_1A: 	dc.b $0, $1
+	dc.b $FC, $0, $0, $13, $FF, $FC
+MenuFont_22: 	dc.b $0, $1
+	dc.b $FC, $0, $0, $14, $FF, $FC
+MenuFont_2A: 	dc.b $0, $1
+	dc.b $FC, $0, $0, $15, $FF, $FC
 	even
