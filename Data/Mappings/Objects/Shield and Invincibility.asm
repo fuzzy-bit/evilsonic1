4cb84b6c138712ce5c2e5c9c7742c92f9344ee54
vladikcomper <rubstov@gmail.com>
2023-05-07 18:31:40 +0200

    Fix shield mappings.

--- Data/Mappings/Objects/Shield and Invincibility.asm
+++ Data/Mappings/Objects/Shield and Invincibility.asm
@@ -13,8 +13,9 @@ Map_Shield_internal:
 @shield2:	dc.w 4
 		dc.b $E8, $A, 0, 0, $FF, $E8
 		dc.b $E8, $A, 0, 9, $00, 0
-@shield1:	dc.w 0,	$A, $10, 0, $FF, $E8
+		dc.b 0,	$A, $10, 0, $FF, $E8
 		dc.b 0,	$A, $10, 9, $00, 0
+@shield1:	dc.w 0
 @shield3:	dc.w 4
 		dc.b $E8, $A, 8, $12, $FF, $E9
 		dc.b $E8, $A, 0, $12, $00, 0
