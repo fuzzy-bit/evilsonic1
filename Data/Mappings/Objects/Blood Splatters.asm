33eb3e64def88b69e12293b6a128c622f2a1b688
Ezra <fuzzy-bit@users.noreply.github.com>
2023-05-30 21:46:49 -0400

    Checkout

---
+++ Data/Mappings/Objects/Blood Splatters.asm
@@ -0,0 +1,6 @@
+Map_Splat:
+Map_Splat_0: 	dc.w Map_Splat_2-Map_Splat
+Map_Splat_2: 	dc.b $0, $2
+	dc.b $F8, $D, $0, $0, $FF, $E0
+	dc.b $F8, $D, $0, $8, $0, $0
+	even
--- Data/Mappings/Objects/BloodSplatter/MZ Pillar.asm
+++
@@ -1,2 +0,0 @@
-Map_MZSplat:
-	even
