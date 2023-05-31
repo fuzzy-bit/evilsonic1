dea679f01ea48ac2968cb207a7f9d69be0c9a4cd
vladikcomper <rubstov@gmail.com>
2023-05-31 23:51:18 +0200

    Sega Screen: Fix Sega Logo Letter object.

--- Objects/Screen-Space/Sega Logo Letters.asm
+++ Objects/Screen-Space/Sega Logo Letters.asm
@@ -5,8 +5,7 @@ SegaLetter:
 		moveq	#0, d0
 		move.b	obRoutine(a0), d0
 		move.w	@Index(pc, d0.w), d1
-		jsr		@Index(pc, d1.w)
-		jmp		DisplaySprite
+		jmp		@Index(pc, d1.w)
 
 ; ===========================================================================
 @Index:	dc.w @Init-@Index
@@ -17,17 +16,16 @@ SegaLetter:
 		move.w	#$120, obX(a0)
 		move.w	#$DE, obScreenY(a0)
 		move.l	#Map_SegaLogo, obMap(a0)
-		move.w	#$200, obGfx(a0)
-		move.b	#4, obRender(a0)
+		move.w	#$200/$20, obGfx(a0)
+		move.b	#0, obRender(a0)
 		move.b	#1, obPriority(a0)
 		move.b	#8, obActWid(a0)
 		move.b	#10,obDelayAni(a0) ; set time delay to 0.5 seconds
-		add.b	#2, obRoutine(a0)
-		rts
+		addq.b	#2, obRoutine(a0)
 
 @Main:
 		jsr 	ScreenObjectFall
-		rts
+		jmp		DisplaySprite
 
 @DeleteObject:
-		jsr 	DeleteObject
+		jmp 	DeleteObject
