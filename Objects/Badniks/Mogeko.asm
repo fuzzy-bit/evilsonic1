a609826329cf9f1240f0a42b4e6710b1f1244890
Ezra <fuzzy-bit@users.noreply.github.com>
2023-06-29 06:24:28 -0400

    Fixed some indents

--- Objects/Badniks/Mogeko.asm
+++ Objects/Badniks/Mogeko.asm
@@ -11,33 +11,33 @@ Mogeko:
 
 ; ===========================================================================
 @Index: dc.w @Init-@Index
-        dc.w @Move-@Index
+		dc.w @Move-@Index
 ; ===========================================================================
 
 @Init:	; Routine 0
-        move.l	#Map_Mogeko, obMap(a0)
+		move.l	#Map_Mogeko, obMap(a0)
 		move.w	#$6000/$20, obGfx(a0)
 		move.b	#4, obRender(a0)
 		move.b	#4, obPriority(a0)
 		move.b	#8, obColType(a0)
-        move.b  #$10, obWidth(a0)
-        move.b  #$14, obHeight(a0)
+		move.b  #$10, obWidth(a0)
+		move.b  #$14, obHeight(a0)
 		move.b	#$14, obActWid(a0)
 
-        ; HELP HOW DOES ANIMATION DELAY WORK
+		; HELP HOW DOES ANIMATION DELAY WORK
 		move.b	#5, obDelayAni(a0)     ; Prosciutto Love Affair
 		addq.b	#2, obRoutine(a0)      ; ~Vows on the Sunset Hill~ 
 
 @Move:
-        lea     (v_player).w, a1
-        move.w  #$420, d0
-        move.w  #$93, d1
-        jsr     ChaseObject
+		lea     (v_player).w, a1
+		move.w  #$420, d0
+		move.w  #$93, d1
+		jsr     ChaseObject
 
-        move.w  #0, obVelY(a0)
-        jsr     SpeedToPos
+		move.w  #0, obVelY(a0)
+		jsr     SpeedToPos
 
-        jsr	    ObjFloorDist
+		jsr	    ObjFloorDist
 		add.w	d1, obY(a0)	            ; Match	object's position with the floor
 
 		subq.b 	#1, obDelayAni(a0)
@@ -45,8 +45,8 @@ Mogeko:
 		beq.s 	@Return
 
 		lea	    (Ani_Mogeko).l, a1
-        jsr     AnimateSprite
+		jsr     AnimateSprite
 		move.b	#5, obTimeFrame(a0)
 
 @Return:
-        rts                             ; Fun fact: Half of this object was written on a plane
+		rts                             ; Fun fact: Half of this object was written on a plane
