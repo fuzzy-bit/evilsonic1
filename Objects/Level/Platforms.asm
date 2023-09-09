129e027bd5abeeb3c8abea4c92943d7c65844d4f
Geeovanni <iamgiovanni.gb@gmail.com>
2023-09-10 00:10:22 +0200

    SYZ Level design progress + Caterkiller bug fix

--- Objects/Level/Platforms.asm
+++ Objects/Level/Platforms.asm
@@ -244,8 +244,8 @@ Plat_Move:
 ; ===========================================================================
 
 @type07:
-		tst.w	$3A(a0)		; is time delay	set?
-		bne.s	@type07_wait	; if yes, branch
+	;	tst.w	$3A(a0)		; is time delay	set?
+	;	bne.s	@type07_wait	; if yes, branch
 		lea	(f_switch).w,a2	; load switch statuses
 		moveq	#0,d0
 		move.b	obSubtype(a0),d0 ; move object type ($x7) to d0
@@ -253,22 +253,23 @@ Plat_Move:
 		tst.b	(a2,d0.w)	; has switch no. d0 been pressed?
 		beq.s	@type07_nomove	; if not, branch
 		move.w	#60,$3A(a0)	; set time delay to 1 second
+		addq.b	#1,obSubtype(a0) ; change to type 08
+
 
 	@type07_nomove:
 		rts	
 
-	@type07_wait:
-		subq.w	#1,$3A(a0)	; subtract 1 from time delay
-		bne.s	@type07_nomove	; if time is > 0, branch
-		addq.b	#1,obSubtype(a0) ; change to type 08
-		rts	
+	; @type07_wait:
+		; subq.w	#1,$3A(a0)	; subtract 1 from time delay
+		; bne.s	@type07_nomove	; if time is > 0, branch
+		; rts	
 ; ===========================================================================
 
 @type08:
 		subq.w	#2,$2C(a0)	; move platform	up
 		move.w	$34(a0),d0
-		subi.w	#$200,d0
-		cmp.w	$2C(a0),d0	; has platform moved $200 pixels?
+		subi.w	#$40,d0
+		cmp.w	$2C(a0),d0	; has platform moved $40 pixels?
 		bne.s	@type08_nostop	; if not, branch
 		clr.b	obSubtype(a0)	; change to type 00 (stop moving)
 
