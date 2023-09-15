7962f71cc99767bf9f461709fbf112a0d2572446
vladikcomper <rubstov@gmail.com>
2023-09-15 20:49:08 +0200

    Fix Address error when displaying invalid sprites (S3K way)

--- Engine/Header.asm
+++ Engine/Header.asm
@@ -1,5 +1,5 @@
 Vectors: 
-		dc.l v_systemstack		; Initial stack pointer value
+		dc.l 0				; Initial stack pointer value (DONTFIX: avoids crash in BuildSprite due to NULL pointer dereference)
 		dc.l EntryPoint 		; Start of program
 		dc.l BusError			; Bus error
 		dc.l AddressError		; Address error (4)
