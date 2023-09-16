5675af554b0335b6b85006f5be11fbf1472bbc60
vladikcomper <rubstov@gmail.com>
2023-09-16 21:52:22 +0200

    Convert DAC table from MegaPCM 1 to MegaPCM 2.

--- Engine/Audio/MegaPCM/Main.asm
+++ Engine/Audio/MegaPCM/Main.asm
@@ -32,46 +32,44 @@ IncludeDAC macro Name,Extension
 ; ---------------------------------------------------------------
 
 SampleTable:
-		DAC_Entry		$04, Kick, pcm					; $81	- Kick
-		DAC_Entry		$0D, Snare, pcm					; $82	- Snare
-		DAC_Entry		$0A, KSnare, pcm				; $83	- Kick+Snare
-		DAC_Entry		$0E, Tom, pcm					; $84	- Hi Tom
-		DAC_Entry		$14, Tom, pcm					; $85	- Mid Tom
-		DAC_Entry		$1A, Tom, pcm					; $86	- Low Tom
-		DAC_Entry		$08, Clap, pcm					; $87	- Hand Clap
-		DAC_Entry		$1B, Timpani, pcm				; $88	- Hi-Timpani
-		DAC_Entry		$21, Timpani, pcm				; $89	- Mid-Timpani
-		DAC_Entry		$26, Timpani, pcm				; $8A	- Low-Timpani
-		DAC_Entry		$12, Tamb, pcm					; $8B	- Tambourine
-		DAC_Entry		$0F, Snare, pcm					; $8C	- Pitched Snare
-		DAC_Entry		$12, Muted_Tamb, pcm			; $8D	- Muted Tamb
-		DAC_Entry		$13, HSL_Kick, pcm				; $8E	- HSL Kick (pitch)
-		DAC_Entry		$08, SegaPCM, pcm				; $8F	- SEGA scream
-		DAC_Entry		$12, HSL_HH, pcm				; $90	- HSL Hit Hat
-		DAC_Entry		$12, HSL_Kick, pcm				; $91	- HSL Kick
-		DAC_Entry		$12, HSL_Snare, pcm				; $92	- HSL Snare
-		DAC_Entry		$12, HSL_KS, pcm				; $93	- HSL KickSnare
-		DAC_Entry		$10, FallSFX, pcm				; $94	- Fall SFX
-		DAC_Entry		$0C, BassCymbal, pcm			; $95	- Bass Cymbal
-		DAC_Entry		$02, MashKick, pcm				; $96	- Mash Kick
-		DAC_Entry		$02, SplatterSnare, pcm			; $97	- Splatter Snare
-		DAC_Entry		$03, Tom2, pcm					; $98	- Tom-2 Hi
-		DAC_Entry		$06, Tom2, pcm					; $99	- Tom-2 Hi-Mid
-		DAC_Entry		$09, Tom2, pcm					; $9A	- Tom-2 Lo-Mid
-		DAC_Entry		$0F, Tom2, pcm					; $9B	- Tom-2 Low
-		DAC_Entry		$03, SonicCD_Yes, pcm+pri+panLR ; $9C	- Sonic CD Yes
-		DAC_Entry		$04, Winrar, pcm				; $9D	- Winrar
-		DAC_Entry		$08, Invalid, pcm				; $9E	- oops
-		DAC_Entry		$04, ChillToTheMax, pcm			; $9F	- Lightning_Splash - Chill To The Max
-		DAC_Entry		$1B, Mogege, pcm+pri			; $A0	- Mogege~
-		DAC_Entry		$1B, Minnaaa, pcm				; $A1	- Minnaaa!
-		DAC_Entry		$1B, Mogegegege, pcm			; $A2	- Mogegegege
-		DAC_Entry		$04, Strike, pcm				; $A3	- Strike
-		DAC_Entry		$04, Scream, pcm				; $A4	- Scream
-		DAC_Entry       $04, Ground, pcm+panLR+pri		; $A5   - Ground Wave
-
-
-MegaPCM_End:
+		dcSample	TYPE_PCM, Kick, 			20454, 					; $81	- Kick
+		dcSample	TYPE_PCM, Snare, 		12258, 					; $82	- Snare
+		dcSample	TYPE_PCM, KSnare, 		14148, 					; $83	- Kick+Snare
+		dcSample	TYPE_PCM, Tom, 			11736, 					; $84	- Hi Tom
+		dcSample	TYPE_PCM, Tom, 			9346, 					; $85	- Mid Tom
+		dcSample	TYPE_PCM, Tom, 			7764, 					; $86	- Low Tom
+		dcSample	TYPE_PCM, Clap, 			15768, 					; $87	- Hand Clap
+		dcSample	TYPE_PCM, Timpani, 		7551, 					; $88	- Hi-Timpani
+		dcSample	TYPE_PCM, Timpani, 		6484, 					; $89	- Mid-Timpani
+		dcSample	TYPE_PCM, Timpani, 		5801, 					; $8A	- Low-Timpani
+		dcSample	TYPE_PCM, Tamb, 			10026, 					; $8B	- Tambourine
+		dcSample	TYPE_PCM, Snare, 		11256, 					; $8C	- Pitched Snare
+		dcSample	TYPE_PCM, Muted_Tamb, 	10026, 					; $8D	- Muted Tamb
+		dcSample	TYPE_PCM, HSL_Kick, 		9674, 					; $8E	- HSL Kick (pitch)
+		dcSample	TYPE_PCM, SegaPCM, 		15768, 					; $8F	- SEGA scream
+		dcSample	TYPE_PCM, HSL_HH, 		10026, 					; $90	- HSL Hit Hat
+		dcSample	TYPE_PCM, HSL_Kick, 		10026, 					; $91	- HSL Kick
+		dcSample	TYPE_PCM, HSL_Snare, 	10026, 					; $92	- HSL Snare
+		dcSample	TYPE_PCM, HSL_KS, 		10026, 					; $93	- HSL KickSnare
+		dcSample	TYPE_PCM, FallSFX, 		10814, 					; $94	- Fall SFX
+		dcSample	TYPE_PCM, BassCymbal, 	12829, 					; $95	- Bass Cymbal
+		dcSample	TYPE_PCM, MashKick, 		23950, 					; $96	- Mash Kick
+		dcSample	TYPE_PCM, SplatterSnare, 23950, 					; $97	- Splatter Snare
+		dcSample	TYPE_PCM, Tom2, 			22095, 					; $98	- Tom-2 Hi
+		dcSample	TYPE_PCM, Tom2, 			17808, 					; $99	- Tom-2 Hi-Mid
+		dcSample	TYPE_PCM, Tom2, 			14914, 					; $9A	- Tom-2 Lo-Mid
+		dcSample	TYPE_PCM, Tom2, 			11256, 					; $9B	- Tom-2 Low
+		dcSample	TYPE_PCM, SonicCD_Yes, 	22095, FLAGS_PRIORITY 	; $9C	- Sonic CD Yes
+		dcSample	TYPE_PCM, Winrar, 		20454, 					; $9D	- Winrar
+		dcSample	TYPE_PCM, Invalid, 		15768, 					; $9E	- oops
+		dcSample	TYPE_PCM, ChillToTheMax, 20454, 				; $9F	- Lightning_Splash - Chill To The Max
+		dcSample	TYPE_PCM, Mogege, 		7551, FLAGS_PRIORITY	; $A0	- Mogege~
+		dcSample	TYPE_PCM, Minnaaa, 		7551, 					; $A1	- Minnaaa!
+		dcSample	TYPE_PCM, Mogegegege, 	7551, 					; $A2	- Mogegegege
+		dcSample	TYPE_PCM, Strike, 		20454, 					; $A3	- Strike
+		dcSample	TYPE_PCM, Scream, 		20454, 					; $A4	- Scream
+		dcSample	TYPE_PCM, Ground, 		20454, FLAGS_PRIORITY	; $A5   - Ground Wave
+SampleTable_End:
 
 ; ---------------------------------------------------------------
 ; DAC Sample Files
