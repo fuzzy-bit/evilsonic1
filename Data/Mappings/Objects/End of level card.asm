84bfc149908ce458e5aee3748e772a6a08ea57fe
Geeovanni <iamgiovanni.gb@gmail.com>
2023-09-18 20:43:24 +0200

    Fixed HUD not displaying correctly in SBZ

--- Data/Mappings/Objects/End of level card.asm
+++ Data/Mappings/Objects/End of level card.asm
@@ -1,14 +1,14 @@
-Map_c19d:
-Map_c19d_0: 	dc.w Map_c19d_12-Map_c19d
-Map_c19d_2: 	dc.w Map_c19d_56-Map_c19d
-Map_c19d_4: 	dc.w Map_c19d_82-Map_c19d
-Map_c19d_6: 	dc.w Map_c19d_9C-Map_c19d
-Map_c19d_8: 	dc.w Map_c19d_BC-Map_c19d
-Map_c19d_A: 	dc.w Map_c19d_DC-Map_c19d
-Map_c19d_C: 	dc.w Map_c19d_DE-Map_c19d
-Map_c19d_E: 	dc.w Map_c19d_E0-Map_c19d
-Map_c19d_10: 	dc.w Map_c19d_E2-Map_c19d
-Map_c19d_12: 	dc.b $0, $B
+Map_2c1c:
+Map_2c1c_0: 	dc.w Map_2c1c_12-Map_2c1c
+Map_2c1c_2: 	dc.w Map_2c1c_56-Map_2c1c
+Map_2c1c_4: 	dc.w Map_2c1c_82-Map_2c1c
+Map_2c1c_6: 	dc.w Map_2c1c_9C-Map_2c1c
+Map_2c1c_8: 	dc.w Map_2c1c_BC-Map_2c1c
+Map_2c1c_A: 	dc.w Map_2c1c_DC-Map_2c1c
+Map_2c1c_C: 	dc.w Map_2c1c_DE-Map_2c1c
+Map_2c1c_E: 	dc.w Map_2c1c_E0-Map_2c1c
+Map_2c1c_10: 	dc.w Map_2c1c_E2-Map_2c1c
+Map_2c1c_12: 	dc.b $0, $B
 	dc.b $F8, $5, $0, $75, $FF, $A4
 	dc.b $F8, $5, $0, $65, $FF, $B3
 	dc.b $F8, $5, $0, $61, $FF, $C2
@@ -20,7 +20,7 @@ Map_c19d_12: 	dc.b $0, $B
 	dc.b $F8, $5, $0, $45, $0, $2C
 	dc.b $F8, $5, $0, $61, $0, $3B
 	dc.b $F8, $5, $0, $75, $0, $4A
-Map_c19d_56: 	dc.b $0, $7
+Map_2c1c_56: 	dc.b $0, $7
 	dc.b $F8, $5, $0, $75, $FF, $CA
 	dc.b $F8, $5, $0, $7D, $FF, $DA
 	dc.b $F8, $5, $0, $69, $FF, $EA
@@ -28,25 +28,25 @@ Map_c19d_56: 	dc.b $0, $7
 	dc.b $F8, $5, $0, $3D, $0, $B
 	dc.b $F8, $5, $0, $5D, $0, $1C
 	dc.b $F8, $5, $0, $3D, $0, $2B
-Map_c19d_82: 	dc.b $0, $4
+Map_2c1c_82: 	dc.b $0, $4
 	dc.b $F8, $D, $1, $5F, $FF, $B0
 	dc.b $F8, $1, $1, $77, $FF, $D0
 	dc.b $F8, $9, $1, $79, $0, $18
 	dc.b $F8, $D, $1, $7F, $0, $30
-Map_c19d_9C: 	dc.b $0, $5
+Map_2c1c_9C: 	dc.b $0, $5
 	dc.b $F8, $D, $1, $6F, $FF, $B0
-	dc.b $F8, $D, $80, $13, $FF, $D9
+	dc.b $F8, $D, $0, $13, $FF, $D9
 	dc.b $F8, $1, $1, $5F, $FF, $F9
-	dc.b $F8, $D, $FF, $FD, $0, $28
+	dc.b $F8, $D, $1F, $FD, $0, $28
 	dc.b $F8, $1, $1, $85, $0, $48
-Map_c19d_BC: 	dc.b $0, $5
+Map_2c1c_BC: 	dc.b $0, $5
 	dc.b $F8, $D, $1, $67, $FF, $B0
 	dc.b $F8, $D, $0, $13, $FF, $D9
 	dc.b $F8, $1, $1, $5F, $FF, $F9
 	dc.b $F8, $D, $0, $5, $0, $28
 	dc.b $F8, $1, $1, $85, $0, $48
-Map_c19d_DC: 	dc.b $0, $0
-Map_c19d_DE: 	dc.b $0, $0
-Map_c19d_E0: 	dc.b $0, $0
-Map_c19d_E2: 	dc.b $0, $0
+Map_2c1c_DC: 	dc.b $0, $0
+Map_2c1c_DE: 	dc.b $0, $0
+Map_2c1c_E0: 	dc.b $0, $0
+Map_2c1c_E2: 	dc.b $0, $0
 	even
