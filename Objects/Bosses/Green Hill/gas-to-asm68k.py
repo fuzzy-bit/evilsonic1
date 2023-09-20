27a0246a90e1709b3c7d07813fa79125c82c7fac
vladikcomper <rubstov@gmail.com>
2023-09-20 02:57:37 +0200

    gas-to-asm68k: Implement MOVEM and external BRA/BSR conversion.

--- Objects/Bosses/Green Hill/gas-to-asm68k.py
+++ Objects/Bosses/Green Hill/gas-to-asm68k.py
@@ -15,11 +15,38 @@ def convertOperand(operand: str) -> str:
 	return operand
 
 
+def convertMovemOperands(operands: list[str]) -> list[str]:
+	sregs = [
+		'd0','d1','d2','d3','d4','d5','d6','d7',
+		'a0','a1','a2','a3','a4','a5','a6','a7',
+	]
+
+	if operands[1].startswith('#'):
+		regVal = int(operands[1][1:])
+		regs = []
+		for i in range(0, 16):
+			if (regVal >> i) & 1:
+				regs.append(sregs[i])
+		operands[1] = '/'.join(regs)
+
+	if operands[0].startswith('#'):
+		regVal = int(operands[0][1:])
+		regs = []
+		for i in range(15, -1, -1):
+			if (regVal >> i) & 1:
+				regs.append(sregs[15-i])
+		operands[0] = '/'.join(regs)
+
+	return operands;
+
+
 with open(input_file, 'r') as input_stream:
 	with open(output_file, 'w') as output_stream:
 		for raw_line in input_stream:
 			# Filter out GCC comments
-			line = raw_line.split('|')[0]
+			split_line = raw_line.split('|')
+			line = split_line[0]
+			comment = None if len(split_line) == 1 else split_line[1]
 
 			# If line is a directive
 			if line.startswith(' ') or line.startswith('\t') and line.strip().startswith('.'):
@@ -38,6 +65,14 @@ with open(input_file, 'r') as input_stream:
 				# Replace JEQ, JNE, ... with BEQ, BNE, ...
 				if opcode not in ('jmp', 'jsr') and opcode.startswith('j'):
 					opcode = 'b' + opcode[1:]
+
+				# Replace assembly calls using "BRA" and "BSR" to "JMP" and "JSR"
+				if opcode in ('bra', 'bsr') and operands[0].endswith('__cdecl'):
+					opcode = 'jmp' if opcode == 'bra' else 'jsr'
+
+				# Convert MOVEM.L/.W
+				if opcode.startswith('movem'):
+					operands = convertMovemOperands(operands)
 
 				# Replace %d0, %a0, ... with d0, a0, ...
 				operands = list(map(convertOperand, operands))
