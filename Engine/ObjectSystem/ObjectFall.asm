92f0689acaa4b50447c473f153c265425dbb7d08
vladikcomper <rubstov@gmail.com>
2023-09-25 04:48:28 +0200

    Speed up SpeedToPos

--- Engine/ObjectSystem/ObjectFall.asm
+++ Engine/ObjectSystem/ObjectFall.asm
@@ -3,20 +3,13 @@
 ; ---------------------------------------------------------------------------
 
 ObjectFall:
-		move.l	obX(a0),d2
-		move.l	obY(a0),d3
-		move.w	obVelX(a0),d0
-		ext.l	d0
-		asl.l	#8,d0
-		add.l	d0,d2
-		move.w	obVelY(a0),d0
+		movem.w	obVelX(a0),d2-d3
+		asl.l	#8, d2
+		add.l	d2, obX(a0)
+		asl.l	#8, d3
+		add.l	d3, obY(a0)
 		addi.w	#$38,obVelY(a0)	; increase vertical speed
-		ext.l	d0
-		asl.l	#8,d0
-		add.l	d0,d3
-		move.l	d2,obX(a0)
-		move.l	d3,obY(a0)
-		rts	
+		rts
 
 ; for screen-space objects =P
 ScreenObjectFall:
