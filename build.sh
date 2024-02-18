f883aa85f2de577c369c6e20c2fa592f9939c181
Ezra <fuzzy-bit@users.noreply.github.com>
2024-02-18 01:20:04 -0500

    Clean up the root directory a bit

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 set -eu
 
-./make-cpp.py
+./.build/make-cpp.py
 
 echo RELEASE build: Assembling and linking...
 wine .build/asm68k.exe /g /l /k /m /o ws+,op+,os+,ow+,oz+,oaq+,osq+,omq+,ae-,v+ sonic.asm, .build/log/sonic.obj, , .build/log/sonic.lst
